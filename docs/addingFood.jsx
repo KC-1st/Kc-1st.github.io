877c11865aec7796e1945771ff2eb390f616c990
KC-1st <kitancoker@gmail.com>
2026-07-01 09:02:19 +0100

    making alert condition controlled

--- docs/addingFood.jsx
+++ docs/addingFood.jsx
@@ -1,2 +1,6 @@
-const button = documet.querySelector("#whatFood")
-    button.addEventListener("click")
+const button = document.querySelector("#whatFood")
+    button.addEventListener("click", function() {
+        // need to say function() follwed by parenthesis 
+        // so that alert is condition controlled
+        alert("button works!")
+    }
