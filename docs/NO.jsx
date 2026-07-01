b767d95b440ea78659e382fa1e123fb1494ded02
KC-1st <kitancoker@gmail.com>
2026-07-01 09:35:53 +0100

    delted old jsx file and replaced it with a js file

---
+++ docs/NO.jsx
@@ -0,0 +1,6 @@
+// const button = document.querySelector("#whatFood")
+//     button.addEventListener("click", function() {
+//         // need to say function() follwed by parenthesis 
+//         // so that alert is condition controlled
+//         alert("button works!")
+//     })
