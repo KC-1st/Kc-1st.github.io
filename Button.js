59ec191a44aa899c7b36baf528f22079fbd0ce5b
KC-1st <kitancoker@gmail.com>
2026-07-02 18:04:22 +0100

    removing indentation

--- Button.js
+++ Button.js
@@ -1,6 +1,7 @@
 const button = document.querySelector("#button1")
-    button.addEventListener("click", function() {
-        // need to say function() follwed by parenthesis 
-        // so that alert is condition controlled
-        alert("button works!")
-    })
+
+button.addEventListener("click", function() {
+    // need to say function() follwed by parenthesis 
+    // so that alert is condition controlled
+    alert("button works!")
+})
