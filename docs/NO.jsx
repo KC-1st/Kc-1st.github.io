9eef916436574cc8e0ec462470f8dd6051edeb1b
KC-1st <kitancoker@gmail.com>
2026-07-02 15:53:07 +0100

    Forgot about a random file I made earlier, have now deleted it

--- docs/NO.jsx
+++
@@ -1,6 +0,0 @@
-// const button = document.querySelector("#whatFood")
-//     button.addEventListener("click", function() {
-//         // need to say function() follwed by parenthesis 
-//         // so that alert is condition controlled
-//         alert("button works!")
-//     })
