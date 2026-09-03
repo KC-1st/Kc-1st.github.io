8bb93795b29989a2ae7abbd7b60ea29ded7ff402
KC-1st <kitancoker@gmail.com>
2026-09-03 10:31:16 +0100

    updating my html folder

--- Script.js
+++ Script.js
@@ -4,4 +4,11 @@ button.addEventListener("click", function() {
     // need to say function() follwed by parenthesis 
     // so that alert is condition controlled
     alert("button works!")
+})  
+
+const MyInput = document.querySelector("#text1")
+
+myInput.addEventListener("change", function(){
+    alert(MyInput.value)
 })
+
